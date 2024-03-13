e555a4750d7cc0d481b73a00c246bd0af1c3485b
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-13 16:59:14 +0200

    added

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/style.css">
-  <title>Products</title>
-</head>
-<body>
-  <header>
-    <p>BestShopVHQ</p>
-    @auth
-      <div>
-        <p>Wsp, {{ auth()->user()->name }}</p>
-        <form action="/logout" method="post">
-        @csrf
-          <button>Log out</button>
-        </form>
-      </div>
-    @endauth
-    @guest
-      <div>
-        <p><a href="/login">Log in</a></p>
-        <p><a href="/register">Sign up</a></p>
-      </div>
-    @endguest
-  </header>
+<x-layout>
   <main>
     <h1>Products</h1>
       <a href="/products/create">Uztaisīt jaunu</a>
@@ -39,5 +12,4 @@
       </article>
       @endforeach
   </main>
-</body>
-</html>
+</x-layout>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{$product->name}}</title>
-</head>
-<body>
+<x-layout>
   <main>
     <h1>{{$product->name}}</h1>
     <img src={{$product->imageURL}} alt="{{$product->name}}" width="200px"/>
@@ -20,5 +12,4 @@
       <button>❌</button>
     </form>
   </main>
-</body>
-</html>
+</x-layout>
